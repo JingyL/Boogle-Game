c6f3cbdd9a807dd602bb04a2213ad646f1e31fcd
Jingyi Lin <linjingyikayla@gmail.com>
2021-11-04 13:51:28 -0400

    create files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template, jsonify, session
+from boggle import Boggle
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "fdfgkjtjkkg45yfdb"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+boggle_game = Boggle()
+
+@app.route("/board")
+def show_board():
+    board = boggle_game.make_board()
+    return render_template("/board.html",board = board)
+
+
